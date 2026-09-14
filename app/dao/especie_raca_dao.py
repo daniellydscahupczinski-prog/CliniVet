264224f0beffc8b41ec9294963647d0963c51a56
ALANA MILK DA ROSA <05126535005@senacrs.edu.br>
2026-09-14 20:15:10 -0300

    parte 4

---
+++ app/dao/especie_raca_dao.py
@@ -0,0 +1,55 @@
+from app.models.especie_raca import Especie_Raca
+from app.dao.dao import DAO
+
+
+class Especie_Raca_DAO(DAO):
+
+    def save(self, especie_id, raca_id):
+
+        especie_raca = Especie_Raca(
+            especie_id,
+            raca_id
+        )
+
+        sql = """
+            INSERT INTO ESPECIE_RACA
+            (ESPECIE_ID, RACA_ID)
+            VALUES (%s, %s)
+        """
+
+        valores = (
+            especie_raca.especie_id,
+            especie_raca.raca_id
+        )
+
+        self.cursor.execute(sql, valores)
+        self.conn.commit()
+
+    def get_by_especie(self, especie_id):
+
+        sql = """
+            SELECT RACA_ID
+            FROM ESPECIE_RACA
+            WHERE ESPECIE_ID = %s
+        """
+
+        self.cursor.execute(sql, (especie_id,))
+
+        return self.cursor.fetchall()
+
+    def delete(self, especie_id, raca_id):
+
+        sql = """
+            DELETE FROM ESPECIE_RACA
+            WHERE ESPECIE_ID = %s
+            AND RACA_ID = %s
+        """
+
+        self.cursor.execute(
+            sql,
+            (especie_id, raca_id)
+        )
+
+        self.conn.commit()
+
+        return self.cursor.rowcount > 0
